e3002cf06f021b8f7b093559382da0d04dfa987d
Malmass <Malmass>
2018-05-31 11:36:37 +0200

    Initial Commit

---
+++ src/calculatrice/Calculatrice.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatrice;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+/**
+ *
+ * @author malmass
+ */
+public class Calculatrice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Que voulez vous faire ?\n");
+        System.out.println("-1 + \n");
+        System.out.println("-2 - \n");
+        System.out.println("-3 x \n");
+        System.out.println("-4 % \n");
+        System.out.println("-5 Des crêpes \n");
+        System.out.println("-6 Rien \n");
+        //BufferedReader in = new BufferedReader(input);
+    }
+}
